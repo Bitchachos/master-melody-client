17c6418dceb046840723b0853bd0f1f0ffe2fecf
Lukecin <dluqueru@gmail.com>
2022-05-20 11:53:23 +0200

    added Navbar component and Homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
+
+function Navbar(){
+    return (
+        <nav className="Navbar">
+            <NavLink to="#">Home</NavLink>
+            <NavLink to="#">Song List</NavLink>
+            <NavLink to="#">Practice Rooms</NavLink>
+            <NavLink to="#">Register</NavLink>
+            <NavLink to="#">Login</NavLink>
+        </nav>
+    )
+  }
+
+export default Navbar;
