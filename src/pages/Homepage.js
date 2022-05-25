efa681570d1690af18301b327b089638f0cff837
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-05-25 12:10:21 +0200

    add logout func and button

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import "./Homepage.css"
 function Homepage(props){
     return (
         <>
-        <h1>Welcome to Master Melody, {props.user}!</h1>
+        <h1>Welcome to Master Melody!</h1>
         <h2 className="actually-learn">... where you <i>actually</i> learn how to play music!</h2>
         </>
     )
