55020e961b93531d1d05a871cb03fba277c36528
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-05-27 00:48:22 +0200

    modify font colors to fit dark mode

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import "./Homepage.css"
 function Homepage(props){
 
     return (
-        <div className="Homepage">
+        <div className={"Homepage"}>
         <h1>Welcome to Master Melody!</h1>
         <h2 className="actually-learn">... where you <i>actually</i> &nbsp;learn how to play music!</h2>
         </div>
