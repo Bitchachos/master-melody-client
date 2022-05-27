7386eb53b6fa837073b0ce12e667e220e6a90761
Lukecin <dluqueru@gmail.com>
2022-05-27 13:53:57 +0200

    add video loop for homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,12 @@
-import "./Homepage.css"
+import "./Homepage.css";
+import video from "../videos/master-melody.mp4";
+import ReactPlayer from 'react-player'
 
 function Homepage(props){
 
     return (
         <div className={"Homepage"}>
+        <ReactPlayer className="video" playing url={ video } autoPlay={true} muted loop/>
         <h1>Welcome to Master Melody!</h1>
         <h2 className="actually-learn">... where you <i>actually</i> &nbsp;learn how to play music!</h2>
         </div>
